d91b81197cafcc59a380348a0c5e0c00c72713e4
Agata <agata.react@gmail.com>
2023-02-12 12:18:28 +0100

    Filter option added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -6,21 +5,14 @@ import './ExpenseItem.css'
 
 function ExpenseItem(props) {
 
-	const [title, setTitle] = useState(props.title);
-
-	const clickHandler = () => {
-		setTitle('updated');
-		console.log('clicked!');
-	}
 
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__description '>
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
 			</div>
-			<button onClick={clickHandler}>Click me</button>
 		</Card>
 	);
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,28 @@
+import { useState } from 'react'
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
 import './Expenses.css'
 
 function Expenses(props) {
 
+    const [filteredYear, setFilteredYear] = useState('2020')
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear)
+    }
+    const filteredExpenses = props.expenses.filter(expense => expense.date.getFullYear().toString() === filteredYear)
+
 
     return (
-        <Card className='expenses'>
-            <ExpenseItem
-                title={props.expenses[0].title}
-                amount={props.expenses[0].amount}
-                date={props.expenses[0].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                title={props.expenses[1].title}
-                amount={props.expenses[1].amount}
-                date={props.expenses[1].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                title={props.expenses[2].title}
-                amount={props.expenses[2].amount}
-                date={props.expenses[2].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                title={props.expenses[3].title}
-                amount={props.expenses[3].amount}
-                date={props.expenses[3].date}
-            ></ExpenseItem>
-        </Card>
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {/* {props.expenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)} */}
+                {filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
+
+            </Card>
+        </div>
     )
 }
 
